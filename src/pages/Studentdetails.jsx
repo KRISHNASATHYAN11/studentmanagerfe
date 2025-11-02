94d403952050f4047ccc98222a4eb32f015f6ae7
krishnasathyan <krishnasathyan11@gmail.com>
2025-11-02 22:12:04 +0530

    edited container-fluid

--- src/pages/Studentdetails.jsx
+++ src/pages/Studentdetails.jsx
@@ -18,7 +18,7 @@ const Studentdetails = () => {
 
   const [studentDetails, setStudentDetails] = useState([]);
   const [editData, setEditData] = useState({
-    Id: "", 
+    Id: "",
     Name: "",
     Course: "",
     Grade: "",
@@ -72,135 +72,143 @@ const Studentdetails = () => {
 
   return (
     <>
-      <h1 className="text-center mt-3">STUDENT MANAGEMENT</h1>
-      <div className="container p-4 border text-center shadow">
-        <div className="row justify-content-center mt-3">
-          <div className="col-md-6">
-            <input
-              value={studentData.Id}
-              onChange={(e) =>
-                setStudentData({ ...studentData, Id: e.target.value })
-              }
-              className="form-control w-75 mt-2"
-              placeholder="Id"
-              type="text"
-            />
-            <input
-              value={studentData.Name}
-              onChange={(e) =>
-                setStudentData({ ...studentData, Name: e.target.value })
-              }
-              className="form-control w-75 mt-2"
-              placeholder="Enter Student Name"
-              type="text"
-            />
-            <input
-              value={studentData.Course}
-              onChange={(e) =>
-                setStudentData({ ...studentData, Course: e.target.value })
-              }
-              className="form-control w-75 mt-2"
-              placeholder="Enter Course"
-              type="text"
-            />
-            <input
-              value={studentData.Grade}
-              onChange={(e) =>
-                setStudentData({ ...studentData, Grade: e.target.value })
-              }
-              className="form-control w-75 mt-2"
-              placeholder="Grade"
-              type="text"
-            />
-            <button onClick={createClick} className="btn btn-success mt-3 ms-5">
-              SUBMIT
-            </button>
+      <div className="container-fluid">
+        {" "}
+        <h1 className="text-center mt-3">STUDENT MANAGEMENT</h1>
+        <div className="container p-4 border text-center shadow">
+          <div className="row justify-content-center mt-3">
+            <div className="col-md-6">
+              <input
+                value={studentData.Id}
+                onChange={(e) =>
+                  setStudentData({ ...studentData, Id: e.target.value })
+                }
+                className="form-control w-75 mt-2"
+                placeholder="Id"
+                type="text"
+              />
+              <input
+                value={studentData.Name}
+                onChange={(e) =>
+                  setStudentData({ ...studentData, Name: e.target.value })
+                }
+                className="form-control w-75 mt-2"
+                placeholder="Enter Student Name"
+                type="text"
+              />
+              <input
+                value={studentData.Course}
+                onChange={(e) =>
+                  setStudentData({ ...studentData, Course: e.target.value })
+                }
+                className="form-control w-75 mt-2"
+                placeholder="Enter Course"
+                type="text"
+              />
+              <input
+                value={studentData.Grade}
+                onChange={(e) =>
+                  setStudentData({ ...studentData, Grade: e.target.value })
+                }
+                className="form-control w-75 mt-2"
+                placeholder="Grade"
+                type="text"
+              />
+              <button
+                onClick={createClick}
+                className="btn btn-success mt-3 ms-5"
+              >
+                SUBMIT
+              </button>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="container mt-5">
-        <table className="table">
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Student Name</th>
-              <th>Course</th>
-              <th>Grade</th>
-              <th>EDIT</th>
-              <th>DELETE</th>
-            </tr>
-          </thead>
-          <tbody>
-            {studentDetails.map((student) => (
-              <tr key={student.id}>
-                <td>{student.Id}</td>
-                <td>{student.Name}</td>
-                <td>{student.Course}</td>
-                <td>{student.Grade}</td>
-                <td>
-                  <button
-                    onClick={() => onEditShowClick(student)}
-                    className="btn btn-success ms-2"
-                  >
-                    EDIT
-                  </button>
-                </td>
-                <td>
-                  <button
-                    onClick={() => onDeleteClick(student.id)}
-                    className="btn btn-danger ms-2"
-                  >
-                    DELETE
-                  </button>
-                </td>
+        <div className="container mt-5">
+          <table className="table">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Student Name</th>
+                <th>Course</th>
+                <th>Grade</th>
+                <th>EDIT</th>
+                <th>DELETE</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {studentDetails.map((student) => (
+                <tr key={student.id}>
+                  <td>{student.Id}</td>
+                  <td>{student.Name}</td>
+                  <td>{student.Course}</td>
+                  <td>{student.Grade}</td>
+                  <td>
+                    <button
+                      onClick={() => onEditShowClick(student)}
+                      className="btn btn-success ms-2"
+                    >
+                      EDIT
+                    </button>
+                  </td>
+                  <td>
+                    <button
+                      onClick={() => onDeleteClick(student.id)}
+                      className="btn btn-danger ms-2"
+                    >
+                      DELETE
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Edit Student</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <input
+              value={editData.Id}
+              onChange={(e) => setEditData({ ...editData, Id: e.target.value })}
+              className="form-control border-success mt-2"
+              type="text"
+            />
+            <input
+              value={editData.Name}
+              onChange={(e) =>
+                setEditData({ ...editData, Name: e.target.value })
+              }
+              className="form-control border-success mt-2"
+              type="text"
+            />
+            <input
+              value={editData.Course}
+              onChange={(e) =>
+                setEditData({ ...editData, Course: e.target.value })
+              }
+              className="form-control border-success mt-2"
+              type="text"
+            />
+            <input
+              value={editData.Grade}
+              onChange={(e) =>
+                setEditData({ ...editData, Grade: e.target.value })
+              }
+              className="form-control border-success mt-2"
+              type="text"
+            />
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={onEditClick}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </div>
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Edit Student</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <input
-            value={editData.Id}
-            onChange={(e) => setEditData({ ...editData, Id: e.target.value })}
-            className="form-control border-success mt-2"
-            type="text"
-          />
-          <input
-            value={editData.Name}
-            onChange={(e) => setEditData({ ...editData, Name: e.target.value })}
-            className="form-control border-success mt-2"
-            type="text"
-          />
-          <input
-            value={editData.Course}
-            onChange={(e) =>
-              setEditData({ ...editData, Course: e.target.value })
-            }
-            className="form-control border-success mt-2"
-            type="text"
-          />
-          <input
-            value={editData.Grade}
-            onChange={(e) =>
-              setEditData({ ...editData, Grade: e.target.value })
-            }
-            className="form-control border-success mt-2"
-            type="text"
-          />
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={onEditClick}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
     </>
   );
 };
